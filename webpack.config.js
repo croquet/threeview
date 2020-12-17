74d3f6f774845a35661a9b664d0e5299e8825aaa
Vanessa Freudenberg <vanessa@codefrau.net>
2020-12-16 22:21:37 -0800

    switch to webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const fetch = require('node-fetch');
+const path = require('path');
+
+module.exports = {
+    entry : './src/threeview.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name]-[contenthash:8].js',
+        chunkFilename: 'chunk-[name]-[contenthash:8].js',
+    },
+    devServer: {
+        disableHostCheck: true,
+        contentBase: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        port: 9009
+    },
+    // use Croquet loaded via <script>
+    externals: {
+        "@croquet/croquet": "Croquet",
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: 'src/index.html',   // input
+            filename: 'index.html',   // output filename in dist/
+            templateParameters: async () => {
+                const response = await fetch('https://croquet.io/sdk/croquet-latest-pre.txt');
+                if (!response.ok) throw Error(`${response.status} ${response.statusText} ${response.url}`);
+                const body = await response.text();
+                return { 'latest_pre': body.trim() };
+            },
+        }),
+    ],
+};
